76e0b80ca3a0e866934dd8754ed3aa4f32245696
neo <neooen891126@gmail.com>
2017-10-24 15:54:55 +0800

    first commit

---
+++ CH/CH3_LAB1/CH3_LAB1/a.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Odd(int u);
+int Even(int u);
+int Totalsum(int u);
+
+int main(void)
+{
+	int n, sum;
+	char addchoice , i;
+	printf("1+2+...+n=?請輸入n=");
+	scanf_s("%d", &n ,1);
+	scanf_s("%c", &i ,1);
+	fflush(stdin);
+	printf("要做奇數和(O),偶數和(E),還是整數和(I)?請選擇");
+	scanf_s("%c", &addchoice,1);
+	switch (addchoice)
+	{
+	case 'O':
+		sum = Odd(n);
+		break;
+	case 'E':
+		sum = Even(n);
+		break;
+	case 'I':
+		sum = Totalsum(n);
+		break;
+	default:
+		printf("選擇錯誤\n");
+		return -1;
+	}
+	printf("總合為%d\n", sum);
+	system("pause");
+	return 0;
+}
+
+int Odd(int u)
+{
+	int i, total = 0;
+	for (i = 1; i <= u; i++)
+		if (i % 2 == 1) {
+			total = total + i;
+		}
+	return total;
+}
+
+int Even(int u)
+{
+	int i, total = 0;
+	for (i = 1; i <= u; i++)
+		if (i % 2 == 0)
+	total = total + i;
+	return total;
+}
+
+int Totalsum(int u)
+{
+	return Odd(u) + Even(u);
+}
